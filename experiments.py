cf14fc724dd4d3e1ff2a1b210b88b83927c5dea2
Thaddeus Watts <tedawatts@gmail.com>
2022-05-10 11:40:44 -0700

    ran experiments to test qsort failure

--- experiments.py
+++ experiments.py
@@ -37,14 +37,37 @@ def run_exp(sort_objs, length_range, endpoints):
             with open('data/results.csv', 'a') as file:
                 file.write(newline + '\n')
 
+def run_random_exps(sort_objs,length_range, endpoints):
+    jobs = []
+    for L in length_range:
+        for end in endpoints:
+            for obj in sort_objs:
+                jobs.append((obj, L, str(end)))
+    random.shuffle(jobs)
+    for obj, L, k in jobs:
+        lst = get_random_integers(start=0, stop=k, size=L)
+        print(obj.name, k, L)
+        try:
+            algorithm_,N_,comparisons_,time_,entry_time_ = (str(val) for val in obj.run_exp(lst))
+        except RecursionError:
+            algorithm_,N_,comparisons_,time_,entry_time_ = (str(val) for val in [obj.name,len(lst),0,0,time.time()])
+            print(f'Failure due to recursion limit: {algorithm_}, {N_}, {k}')
+        newline = ','.join((algorithm_,N_,k,comparisons_,time_,entry_time_))
+        if comparisons_ == '0' and time_ == '0':
+            with open('data/failed_results.csv', 'a') as file:
+                file.write(newline + '\n')
+        else:
+            with open('data/results.csv', 'a') as file:
+                file.write(newline + '\n')
+
 
 if __name__ == '__main__':
     sorting_obj_dict = {name:sorting_obj(name) for name in alg_names}
     sorting_obj_dict.pop('default_sort')
     sorting_obj_dict.pop('insertion_sort')
-    sorting_objs = [sorting_obj_dict[name] for name in sorting_obj_dict.keys()]
+    sorting_objs = [sorting_obj('quick_sort')]#[sorting_obj_dict[name] for name in sorting_obj_dict.keys()]
     ranges = [int(val) for val in np.geomspace(100,10**6,285)]
-    ks = [val-1 for val in (10, 10**2, 10**3, 10**5, 10**7)] + [4*val for val in (10, 10**2, 10**3, 10**5, 10**7)]
-    run_exp(sorting_objs, ranges, ks)
+    ks = [int(val) for val in np.geomspace(4,1000,100)]
+    run_random_exps(sorting_objs, ranges, ks)
 
 
